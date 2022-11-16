db5326180292e9d81d9e751a30625ba5c1051732
GabrielPenaHdz <pena.hernandez.gabriel15@gmail.com>
2022-11-16 07:33:06 -0600

    menuUno

---
+++ app/src/main/java/com/example/myoptionsmenu/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myoptionsmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        boolean flag = false;
+        MenuInflater menuInflater;
+        flag = super.onCreateOptionsMenu(menu);
+        menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mi_menu, menu);
+        return flag;
+
+    }
+}
